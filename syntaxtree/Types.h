2aa8a4d9185addccf0937ffcc998b8aa908bfe81
Peter <mokrov.pv@phystech.edu>
2019-10-27 18:57:59 +0300

    Add Types Nodes Implementation

---
+++ syntaxtree/Types.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "Identifier.h"
+#include "SyntaxTreeNode.h"
+
+namespace SyntaxTree{
+
+    class IType : public ISyntaxTreeNode {
+    };
+
+    class IntType : public IType {
+    public:
+        IntType() = default;
+
+        virtual void AcceptVisitor(const IVisitor* visitor) const override { visitor->VisitNode(this); }
+    };
+
+    class  BooleanType : public IType {
+    public:
+        BooleanType() = default;
+
+        virtual void AcceptVisitor(const IVisitor* visitor) const override { visitor->VisitNode(this); }
+    };
+
+    class IntArrayType: public  IType {
+    public:
+        IntArrayType() = default;
+
+        virtual void AcceptVisitor(const IVisitor* visitor) const override { visitor->VisitNode(this); }
+    };
+
+    class IdentifierType : public IType {
+    public:
+        IdentifierType(const Identifier* _identifier): identifier(std::make_unique<Identifier>(_identifier)) {}
+        virtual void AcceptVisitor(const IVisitor* visitor) const override { visitor->VisitNode(this); }
+
+        const Identifier* GetIdentifier() const { return identifier.get(); }
+    private:
+        std::unique_ptr<Identifier> identifier;
+    };
+
+}
+
+
+
+
