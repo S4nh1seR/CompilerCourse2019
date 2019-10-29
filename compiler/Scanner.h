48e3e6d40b986ab9c167e9bec316cefb89f3d1d8
Ivan Ignatev <ignatev.is@phystech.edu>
2019-10-29 05:37:42 +0300

    Add non-terminal types

--- compiler/Scanner.h
+++ compiler/Scanner.h
@@ -8,25 +8,34 @@
 #include "mc_parser.tab.hh"
 #include "location.hh"
 
+#include "syntaxtree/Goal.h"
+
 using Tokens = yy::MC_Parser::token_type;
 
 class MyScanner : public yyFlexLexer {
 public:
     int Process(Tokens token) {
-	std::cout << token << std::endl;
-    return token;
+	    std::cout << token << std::endl;
+        return token;
     };
+
     void Space();
+
     using FlexLexer::yylex;
     virtual int yylex( yy::MC_Parser::semantic_type* const lval, 
  					   yy::MC_Parser::location_type *location1 );
    // YY_DECL defined in mc_lexer.l
    // Method body created by flex in mc_lexer.yy.cc
 
+   void SetSyntaxTree(SyntaxTree::Goal* goal) {
+       syntaxTree.reset(goal);
+   }
 
 private:
    /* yyval ptr */
    yy::MC_Parser::semantic_type *yylval = nullptr;
+
+   std::unique_ptr<SyntaxTree::Goal> syntaxTree;
 };
 
 #endif /* END __MCSCANNER_HPP__ */
