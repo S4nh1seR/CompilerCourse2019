26fe313fee0ad6eb99a9afcc740a310c3c72dcdf
PetrMokrov <mokrov.pv@phystech.edu>
2019-12-16 01:28:14 +0300

    add positive samples test

---
+++ tests/test_positive_spls.sh
@@ -0,0 +1,17 @@
+programms=(BinarySearch BinaryTree Factorial LinearSearch LinkedList QuickSort TreeVisitor)
+touch err_file.txt
+for program in ${programms[*]}
+do
+	${PWD}/build/compiler/compiler ${PWD}/tests/PositiveSamples/${program}.java /dev/null 2> err_file.txt
+        if [ -s err_file.txt ]
+	then
+	printf "%s.java not passed!\n" $program
+	cat err_file.txt
+	rm err_file.txt
+	exit 1
+	else
+	cat err_file.txt	
+	printf "%s.java passed\n" $program
+	fi
+done
+rm err_file.txt
